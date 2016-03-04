e399a7e9c42f34c71456155e2848926a42f8b256
huanglifu <huanlifu@gmail.com>
2016-03-04 17:18:36 +0800

    add winlogdetail

---
+++ raiders/src/main/java/com/huotu/fanmore/pinkcatraiders/ui/raiders/WinLogDetailActivity.java
@@ -0,0 +1,52 @@
+package com.huotu.fanmore.pinkcatraiders.ui.raiders;
+
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.ViewStub;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.huotu.fanmore.pinkcatraiders.R;
+import com.huotu.fanmore.pinkcatraiders.base.BaseApplication;
+import com.huotu.fanmore.pinkcatraiders.ui.base.BaseActivity;
+
+import butterknife.Bind;
+
+public class WinLogDetailActivity extends BaseActivity implements View.OnClickListener, Handler.Callback {
+
+    public
+    Resources resources;
+    public BaseApplication application;
+    public Handler mHandler;
+    public
+    WindowManager wManager;
+    @Bind( R.id.titleLayoutL )
+    RelativeLayout titleLayoutL;
+
+    @Bind ( R.id.stubTitleText )
+    ViewStub stubTitleText;
+
+    @Bind ( R.id.titleLeftImage )
+    ImageView titleLeftImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ri_win_log_detail);
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
